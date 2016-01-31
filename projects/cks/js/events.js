9bdb02c2d5fd8e8f179bb03576cb79f0ae33131c
William <billreithmeyer@gmail.com>
2016-01-31 14:30:47 -0500

    tweaks

---
+++ projects/cks/js/events.js
@@ -0,0 +1,34 @@
+
+
+//$(document).ready(function(){
+//
+//    var _navHeight = $('.header .nav').height();
+//    var _headerHeight = $('.header').height();
+//    var _navTop = _headerHeight - _navHeight - 48;
+//    if ($(document).innerWidth() >= 900){
+//        $(window).scroll(function () {
+//
+//            if ($(document).scrollTop() > (_navTop) ) {
+//                if ($(document).innerWidth() >= 1000){
+//                   $(".header .nav").css('position', 'fixed');
+//                   $(".header .nav").css('top', '0');
+//                   $(".header .nav").css('height', '84px');
+//                   $(".header .nav").css('box-shadow', '0 7px 7px -5px rgba(0,0,0,.6)');
+//                }
+//            }
+//            else
+//            {
+//               $(".header .nav").css('position', 'absolute');
+//               $(".header .nav").css('top', '');
+//               $(".header .nav").css('bottom', '0');
+//               $(".header .nav").css('box-shadow', '');
+//            }
+//        });
+//    }
+//
+//    $( window ).resize(function() {
+//        _navHeight = $('.header .nav').height();
+//        _headerHeight = $('.header').height();
+//        _navTop = _headerHeight - _navHeight - 48;
+//    });
+//});
